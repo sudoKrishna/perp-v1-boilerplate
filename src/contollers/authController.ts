b7a73b85e8d6515511a6035e5173ad70d7a1fad1
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2026-05-21 10:07:31 +0530

    created the authcontroller.ts

---
+++ src/contollers/authController.ts
@@ -0,0 +1,32 @@
+import type { Request, Response } from "express";
+import { createUser , loginUser } from "../services/userService";
+import { prisma } from "../config/db";
+
+export const signup = async(req : Request, res: Response) => {
+   try {
+     const {username , password} = req.body;
+ 
+     const result = await createUser(username , password);
+ 
+     return res.status(201).json(result);
+   } catch (error : any) {
+    return res.status(400).json({
+        message : error.message
+    });
+   }
+}
+
+export const signin = async(req : Request, res :Response) => {
+try {
+      const {username , password } = req.body;
+    
+      const result = await loginUser(username, password);
+    
+      return res.status(200).json(result);
+} catch (error : any) {
+    return res.status(400).json({
+        message : error.message
+    })
+}
+
+}
