868163a0fa74b3a18a09e65aa57cda385844caf8
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2026-05-23 02:05:27 +0530

    create: add liquidationQueue for managing liquidation tasks

---
+++ src/queues/liquidationQueue.ts
@@ -0,0 +1,6 @@
+import { Queue } from "bullmq";
+import { redisConnection } from "../config/redis";
+
+export const liquidationQueue = new Queue("liquidation-queue", {
+  connection: redisConnection,
+});
