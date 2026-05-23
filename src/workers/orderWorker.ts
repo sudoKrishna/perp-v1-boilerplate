5bf9cc0bcc6da2eb12f30c292628f59c1f986947
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2026-05-23 10:07:49 +0530

    refactor(orderWorker.ts): simplify order processing logic and remove transaction handling

--- src/workers/orderWorker.ts
+++ src/workers/orderWorker.ts
@@ -2,86 +2,35 @@ import { Worker } from "bullmq";
 import {prisma}  from "../config/db";
 import { redisConnection } from "../config/redis";
 import { TradeEngin } from "../services/tradeEngine";
+import {PositionService} from "../services/positionService";
+import { createFill } from "../services/fillService";
 
 
-const orderWorker = new Worker(
+const engine = new TradeEngin();
+const positionService = new PositionService;
+
+
+export const orderWorker = new Worker(
     "orderQueue",
     async (job) => {
         const {userId , order} = job.data;
 
-        await prisma.$transaction(async (tx) => {
-            const user = await tx.user.findUnique({
-                where : {userId},
-                include : {
-                    collateral : true,
-                },
-            });
-
-            if(!user?.collateral) {
-                throw new Error(
-                    "collateral not found"
-                );
-            }
-
-            const collateral = user.collateral;
-
-             if (collateral.available < order.margin) {
-              throw new Error("Insufficient collateral");
-              }
-
-            await tx.collateral.update({
-                where : {id: collateral.id,},
-                 data : {available : collateral.available - order.margin, 
-                         locked : collateral.locked + order.margin,
-                 },
-            });
-
-            await tx.order.create({
-                data : {
-                    userId,
-                    market : order.market,
-                    type : order.type,
-                    qty : order.qty,
-                    margin : order.margin,
-                    orderType : order.orderType,
-                    price : order.price,
-                    status : "OPEN" ,
-                },
-            });
-
-            await tx.position.create({
-                data : {
-                    userId,
-                    market : order.market,
-                    type : order.type,
-                    qty : order.qty,
-                    margin : order.margin,
-                    averagePrice : order.price,
-                    liquidationPrice : order.price * 0.5,
-                    pnl : 0,
-                    status : "OPEN",
-                },
-            });
+        const user = await prisma.user.findUnique({
+            where : {userId},
+            include : {collateral : true},
         });
 
-        console.log("order succesfull");
+        if(!user?.collateral) {
+            throw new Error("collateral not found")
+        }
+
+        await prisma.collateral.update({
+            where : {id : user.collateral.id},
+            data : {
+                available : user.collateral.available - order.margin,
+                locked : user.collateral.locked + order.margin
+            },
+        });
+
     },
-    {
-        connection : redisConnection,
-        concurrency : 50,
-    }
-);
-
-orderWorker.on(
-    "completed",
-    (job) => {
-        console.log(`Job ${job.id} completed`);
-    }
-);
-
-orderWorker.on(
-    "failed",
-    (job, err) => {
-        console.error(`job ${job?.id} failed` , err)
-    }
 )
